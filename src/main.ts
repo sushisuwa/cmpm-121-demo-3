246c2087ce1b01b6899d5553370e8517b3cfb4f6
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2024-11-06 13:14:35 -0800

    change main.ts for dialog assgn

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-// todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+const button = document.createElement("button");
+button.innerHTML = "Click here";
+button.addEventListener("click", () => displayMessage());
+app.append(button);
+
+function displayMessage() {
+  const message = document.createElement("p");
+  message.innerHTML = "alert! (you clicked the button)";
+  app.append(message);
+}
